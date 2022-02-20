d42d28adc19fba88ab9ddffd8bde86d2fcd04437
msidler <sidlermatias02@gmail.com>
2022-02-20 16:21:18 -0300

    console logs removed

--- front/js/script.js
+++ front/js/script.js
@@ -2,15 +2,12 @@ import { ethers } from "./ethers.js";
 import { donations, provider } from "./donations.js"
 
 window.ethereum.on("accountsChanged", (accounts) =>{
-    console.log("new account!!!", accounts[0])
     const acc = accounts[0]
-    console.log("length uwu", accounts.length)
     if (accounts.length == 0){
         const newText = `<p class="form_txt" id="currentAdr">Not connected</p>`
         let divToReplace = document.getElementById("currentAdr")
         let frag = document.createRange().createContextualFragment(newText)
         document.getElementById('form').replaceChild(frag, divToReplace)
-        console.log("la puta madre debería llegar")
         return
     }
     const newText = `<p class="form_txt" id="currentAdr">${acc}</p>`
@@ -24,14 +21,12 @@ let textInput;
 window.onload = async () =>{
 try{
     let signer = provider.getSigner()
-    console.log(await signer.getAddress())
     signer = await signer.getAddress()
     const newText = `<p class="form_txt" id="currentAdr">${signer}</p>`
     let divToReplace = document.getElementById("currentAdr")
     let frag = document.createRange().createContextualFragment(newText)
     document.getElementById('form').replaceChild(frag, divToReplace)
 }catch(e){
-    console.log("signer not found", e)
            const newText = `<p class="form_txt" id="currentAdr">Not connected</p>`
         let divToReplace = document.getElementById("currentAdr")
         let frag = document.createRange().createContextualFragment(newText)
@@ -47,7 +42,6 @@ const benefitedsAdr = await donations.GetAllBenefited()
         return
     } 
     let benefitedStruct = await donations.GetBenefitedByAdr(value)
-    console.log("data to change: ",benefitedStruct)
     let data = {
         adr: value,
         desc: benefitedStruct[3],
@@ -66,7 +60,6 @@ buttonStarted.addEventListener("click", async () => {
     try {
         await provider.send("eth_requestAccounts", []);
         const signer = provider.getSigner()
-        console.log(await signer.getAddress())
         document.getElementById("list_projects").scrollIntoView({behavior: 'smooth'})
         const sig = await signer.getAddress()
         const newText = `<p class="form_txt" id="currentAdr">${sig}</p>`
@@ -83,7 +76,6 @@ buttonStarted.addEventListener("click", async () => {
 //listening for new benefiteds
 
 donations.on("NewBenefited", (adr, description, collected, target) => {
-console.log("HOLA??", adr, description, collected, target)
         let data = {
             adr: adr,
             desc: description,
@@ -92,16 +84,13 @@ console.log("HOLA??", adr, description, collected, target)
             input_id: adr
         }
         //if a benefited was found, add it to benefitedList and execute "addProject" function
-        console.log("descrip:", adr, description, collected, target)
         const newProject = addProject(data)
         document.getElementById('list_projects').insertAdjacentHTML("afterbegin", newProject)
-        console.log("andaaaaaaaa")
 })
 donations.on("NewDonation", (donator, benefited, amountReceived, collected, target) => {
     const parent = document.getElementById(benefited)
     const children = parent.querySelector('.project_description').textContent
     
-    console.log(donator, benefited, ethers.utils.formatEther(amountReceived), ethers.utils.formatEther(collected), ethers.utils.formatEther(target))
     let data = {
         adr: benefited,
         desc: children,
@@ -120,7 +109,6 @@ donations.on("NewDonation", (donator, benefited, amountReceived, collected, targ
 })
 
 document.addEventListener("click", async (e) => {
-    console.log(e.target.className)
     const [firstClass, secondClass] = e.target.className.split(" ")
     let signal = false
     let desc = undefined
@@ -128,7 +116,6 @@ document.addEventListener("click", async (e) => {
     if (firstClass === "donate"){
         if (secondClass == "submit"){
             [desc, target] = document.getElementsByClassName("addProject")
-            console.log("MUY BIEN LLEGA HASTA ACA")
             if (desc.value == ""){
                 alert("please, set a description")
                 return
@@ -149,7 +136,6 @@ document.addEventListener("click", async (e) => {
 
         await provider.send("eth_requestAccounts", []);
         let signer = provider.getSigner()
-        console.log(await signer.getAddress())
     
         const txWithSigner = donations.connect(signer)
 
@@ -159,7 +145,6 @@ document.addEventListener("click", async (e) => {
             if (signal == true){
                 signer = await signer.getAddress()
                 
-                console.log("signerrr", signer.toString(), target.value, desc.value)
                 options = {gasLimit:2100000, gasPrice: 8000000000}
                 tx = await txWithSigner.AddBenefited(signer.toString(), ethers.utils.parseEther(target.value), desc.value, options)
                 
@@ -167,12 +152,9 @@ document.addEventListener("click", async (e) => {
                 options = {value: ethers.utils.parseEther(textInput.value), gasLimit:2100000, gasPrice: 8000000000}
                 tx = await txWithSigner.ReceiveDonations(secondClass, options)
             }
-            console.log("tx papa:" ,tx.hash)
             const a = await provider.getTransaction(tx.hash)
             const getBlockNumber = await provider.call(a, a.blockNumber)
-            console.log(getBlockNumber)
         }catch(e){
-            console.log("error acá!!!")
             alert(e.error.message)
             return
         }
@@ -198,9 +180,6 @@ const addProject = (data) => {
 } 
 
 function checkCollected(param){
-    console.log("param")
-    console.log(param)
-    console.log(param == "COMPLETED")
     let res = "ETH"
     if (param == "COMPLETED"){
         res = ""
